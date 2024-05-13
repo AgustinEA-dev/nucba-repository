baddf44f88ab62ce502c7ce0ff8a6c1baf2108fd
unknown <ag_antro@hotmail.com>
2024-05-13 10:59:31 -0300

    último commit

--- Ejercicio-nav/index.js
+++ Ejercicio-nav/index.js
@@ -1,6 +1,23 @@
+const nav = document.querySelector(".nav");
+let lastScrollY = window.scrollY;
 const barsMenu = document.querySelector('#bars-icon')
 const dropDownMenu = document.querySelector('.dropDown-menu')
 
+
+window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+        // console.log('going down')
+        nav.classList.add("nav--hidden");
+        dropDownMenu.classList.add("drop--hidden")
+    } else {
+        // console.log('going up')
+        nav.classList.remove("nav--hidden");
+        dropDownMenu.classList.remove("drop--hidden")
+    }
+
+    lastScrollY = window.scrollY;
+});
+
 barsMenu.addEventListener('click', () => {
-    dropDownMenu.classList.toggle('dropwDown-menu-visibile')
+    dropDownMenu.classList.toggle('dropwDown-visible')
 })
