09e3e43e315242a032520e0deb7dae2d7be2d5c6
unknown <ag_antro@hotmail.com>
2024-10-27 22:21:53 -0300

    last commit

---
+++ backend/Clase 1/script.js
@@ -0,0 +1,22 @@
+const numeroEntero = 10
+const numeroDecimal = 3.25
+const cadenaDeTexto = "Bienvenidos a backend"
+const booleano = true
+const array = ["Leonel", "Zulema", "Nahuel", "Alejo", "Patroclo"]
+
+const objeto = {
+    nombre: "Hector",
+    profesion: "Crack"
+}
+
+const funcion = function () {
+    console.log("Soy una función.")
+}
+
+console.log("Numero entero:", numeroEntero)
+console.log("Numero decimal:", numeroDecimal)
+console.log("Cadena de texto:", cadenaDeTexto)
+console.log("Booleano:", booleano)
+console.log("Array:", array)
+console.log("Objeto:", objeto)
+funcion()
